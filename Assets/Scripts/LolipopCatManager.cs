5fbd290449b24b432ece3ab850c4f99e13e06ddb
jdoneill <39039516+jdoneill@users.noreply.github.com>
2021-04-14 17:12:01 -0700

    lolipop cat changes sign with the tempo of the beat
    Changes are all on the lolipopcat empty game object and the lolipop cat code

---
+++ Assets/Scripts/LolipopCatManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LolipopCatManager : MonoBehaviour
+{
+
+    public GameObject stopCat;
+    float currentTime = 0f;
+    float redTime = 1.5f;
+    float timeReset = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        currentTime += Time.deltaTime;
+
+        if (currentTime > redTime)
+        {
+            stopCat.SetActive(false);
+
+        }
+        else
+        {
+            stopCat.SetActive(true);
+        }
+
+        if (currentTime > timeReset)
+        {
+            currentTime = 0f;
+        }
+
+    }
+}
